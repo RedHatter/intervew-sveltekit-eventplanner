5977348ff2c62d4c0d53f07cdc7c66205676fff1
Ava Johnson <ava.johnson@zohomail.com>
2025-02-28 14:27:10 -0800

    Add loading state to form

--- src/routes/newevent/+page.svelte
+++ src/routes/newevent/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
+	let disabled = $state(false);
 </script>
 
-<form method="POST">
+<form
+	method="POST"
+	use:enhance={() => {
+		disabled = true;
+
+		return async ({ update }) => {
+			await update();
+			disabled = false;
+		};
+	}}
+>
 	<!-- form for creating new event -->
 	<label for="title">Title</label>
-	<input type="text" id="title" name="title" required />
-	<textarea id="description" name="description" rows="4" cols="50" placeholder="Description"
+	<input {disabled} type="text" id="title" name="title" required />
+	<textarea
+		{disabled}
+		id="description"
+		name="description"
+		rows="4"
+		cols="50"
+		placeholder="Description"
 	></textarea>
 	<label for="date">Date</label>
-	<input type="datetime-local" id="date" name="date" required />
-	<button type="submit">Create Event</button>
+	<input {disabled} type="datetime-local" id="date" name="date" required />
+	<button {disabled} type="submit">
+		Create Event
+		{#if disabled}
+			<span class="loading loading-spinner loading-xs"></span>
+		{/if}
+	</button>
 </form>
